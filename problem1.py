b7a718c3f6ea3afe44306ea605a6044078ae26ea
Gavin Ge <gavin.ge23@ycdsbk12.ca>
2021-02-12 15:04:35 +0000

    Finished.

--- problem1.py
+++ problem1.py
@@ -1,1 +1,27 @@
+"""
+-------------------------------------------------------------------------------
+Name:   problem1.py
+Purpose:  Determines if a triangle is right-angled using the inputted sidelengths of the triangle
+ 
+Author: Ge.G
+ 
+Created:  12/02/2021
+------------------------------------------------------------------------------
+"""
+print("******Right-angled triangle determiner")
 
+# get triangle side-lengh info
+sideOne = float(input("Enter the length of side 1: "))
+sideTwo = float(input("Enter the length of side 2: "))
+sideThree = float(input("Enter the length of side 3: "))
+
+# compute squares of sides
+sideOneSquared = sideOne**2
+sideTwoSquared = sideTwo**2
+sideThreeSquared = sideThree**2
+
+# determine  and output if the triangle is a right-angled triangle
+if (sideOneSquared+sideTwoSquared == sideThreeSquared) or (sideTwoSquared+sideThreeSquared == sideOneSquared) or (sideThreeSquared+sideOneSquared == sideTwoSquared):
+  print(f"\nThe triangle with side lengths {sideOne}, {sideTwo}, and {sideThree} form a right-angled triangle.")
+else:
+  print(f"\nThe triangle with side lengths {sideOne}, {sideTwo}, and {sideThree} do not form a right-angled triangle.")
--- problem2.py
+++ problem2.py
@@ -1,1 +1,23 @@
+"""
+-------------------------------------------------------------------------------
+Name:   problem2.py
+Purpose:  Determines if I will go to Europe, California, or stay home based on inputted grades and amount of money earned before the summer
+ 
+Author: Ge.G
+ 
+Created:  12/02/2021
+------------------------------------------------------------------------------
+"""
+print("******Where will I go?******")
 
+# get mark and earnings info
+mark = float(input("Enter your mark: "))
+earnings = float(input("Enter the amount you earned before the summer: "))
+
+# determine and output where I can go
+if mark >= 80 and earnings >= 500:
+  print("You can go to Europe")
+elif mark >= 80:
+  print("You can go to California")
+else:
+  print("You stay home")
